03846ffa854693a3f6604eac259b3ba7a5bf1d17
auooru <auooru@outlook.com>
2021-09-21 10:53:11 +0800

    update ci

--- .github/php-jit.sh
+++ .github/php-jit.sh
@@ -6,4 +6,4 @@ if [ -n "${PHP_JIT}" ]; then
   echo "opcache.enable_cli=1" >> /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
   echo "opcache.jit_buffer_size=64M" >> /usr/local/etc/php/conf.d/docker-php-ext-opcache.ini
   echo ">> php8 jit enable"
-;fi
+fi
