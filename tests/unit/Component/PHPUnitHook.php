53abec8f9ecbc03dbc4ff99becd08709e05cf2c1
auooru <auooru@outlook.com>
2021-11-03 10:53:16 +0800

    添加性能测试工具

--- tests/unit/Component/PHPUnitHook.php
+++ tests/unit/Component/PHPUnitHook.php
@@ -11,6 +11,8 @@ use Imi\Event\EventParam;
 use Imi\Pool\Interfaces\IPoolResource;
 use Imi\Pool\PoolManager;
 use PHPUnit\Runner\BeforeFirstTestHook;
+use Xhgui\Profiler\Profiler;
+use Xhgui\Profiler\ProfilingFlags;
 
 class PHPUnitHook implements BeforeFirstTestHook
 {
@@ -31,6 +33,43 @@ class PHPUnitHook implements BeforeFirstTestHook
                 }
             });
         }, 1);
-        App::run('Imi\Test\Component', TestApp::class);
+        $profiler = new Profiler([
+            'save.handler'        => Profiler::SAVER_STACK,
+            'save.handler.stack'  => [
+                'savers'  => [
+                    Profiler::SAVER_UPLOAD,
+                    Profiler::SAVER_FILE,
+                ],
+                // if saveAll=false, break the chain on successful save
+                'saveAll' => false,
+            ],
+            'profiler.flags' => [
+                ProfilingFlags::CPU,
+                ProfilingFlags::MEMORY,
+                ProfilingFlags::NO_SPANS,
+                // ProfilingFlags::NO_BUILTINS,
+            ],
+            // subhandler specific configs
+            'save.handler.file'   => [
+                'filename' => '/tmp/imi2.xhgui.data.jsonl',
+            ],
+            'save.handler.upload' => [
+                'url'     => 'http://xhgui.test/run/import',
+                'timeout' => 3,
+                'token'   => 'token',
+            ],
+        ]);
+        try {
+            $profiler->enable();
+            try {
+                App::run('Imi\Test\Component', TestApp::class);
+            } catch (\Throwable $exception) {
+                var_dump((string) $exception);
+                throw $exception;
+            }
+        } finally {
+            $profilerData = $profiler->disable();
+            $profiler->save($profilerData);
+        }
     }
 }
